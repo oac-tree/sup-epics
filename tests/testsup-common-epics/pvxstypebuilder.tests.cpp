0eafa503beb5a5128fb1712249c946e2fedeadc6
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-04-29 14:04:17 +0200

    Place for PvxsTypeBuilder tests

---
+++ tests/testsup-common-epics/pvxstypebuilder.tests.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ * Project       : Supervision and automation system EPICS interface
+ *
+ * Description   : Library of SUP common components for EPICS network protocol
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sup/epics/pvxstypebuilder.h"
+
+#include "AnyTypeHelper.h"
+#include "AnyValue.h"
+
+#include <gtest/gtest.h>
+#include <pvxs/data.h>
+#include <pvxs/nt.h>
+
+using namespace ::sup::epics;
+
+class PvxsTypeBuilderTest : public ::testing::Test
+{
+public:
+  pvxs::TypeDef GetPVXSType(const sup::dto::AnyType& any_type)
+  {
+    PvxsTypeBuilder builder;
+    sup::dto::SerializeAnyType(any_type, builder);
+    return builder.GetPVXSType();
+  }
+};
+
+TEST_F(PvxsTypeBuilderTest, ScalarBasedPVXSType)
+{
+  EXPECT_EQ(1, 1);
+}
