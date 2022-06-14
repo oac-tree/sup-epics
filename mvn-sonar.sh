a9c0d090f856338670b1fa1ca7b2eb0e90f0fe9d
Walter Van Herck <walter.vanherck@iter.org>
2022-06-14 15:01:49 +0000

    Disable valgrind (conflicts with softIoc)

--- mvn-sonar.sh
+++ mvn-sonar.sh
@@ -3,6 +3,7 @@
 #+======================================================================
 #+======================================================================
 
-# mvn clean test -Dcoverage
-mvn clean test -Dcoverage -Dvalgrind
+mvn clean test -Dcoverage
+# Valgrind seems to conflict with the softIoc used during unit tests:
+# mvn clean test -Dcoverage -Dvalgrind
 mvn sonar -DupdateSonar -DanalysisMode=publish -Dlogin=f212cb1196e89c8984098d31a4fa6c9a43cc7d07
