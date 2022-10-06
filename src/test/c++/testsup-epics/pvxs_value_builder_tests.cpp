7466ccee3634d1c2b81b7b9e1ca0d03a0197d70b
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-10-06 15:48:39 +0200

    Unit test for NestedStruct

--- src/test/c++/testsup-epics/pvxs_value_builder_tests.cpp
+++ src/test/c++/testsup-epics/pvxs_value_builder_tests.cpp
@@ -52,16 +52,20 @@ TEST_F(PvxsValueBuilderTests, FromEmptyType)
 
 TEST_F(PvxsValueBuilderTests, FromScalar)
 {
+  // original any_value
   sup::dto::AnyValue any_value{sup::dto::SignedInteger32Type, 42};
 
+  // initializing builder
   auto pvxs_type = ::pvxs::TypeDef(::pvxs::TypeCode::Int32);
   PvxsValueBuilder builder(pvxs_type);
 
+  // building PVXS value
   builder.ScalarProlog(&any_value);
   builder.ScalarEpilog(&any_value);
 
   auto pvxs_value = builder.GetPVXSValue();
 
+  // validating pvxs_value
   EXPECT_TRUE(pvxs_value.valid());
   EXPECT_EQ(pvxs_value.type(), ::pvxs::TypeCode::Int32);
   EXPECT_EQ(pvxs_value.as<int32_t>(), 42);
@@ -71,11 +75,14 @@ TEST_F(PvxsValueBuilderTests, FromScalar)
 
 TEST_F(PvxsValueBuilderTests, StructWithSingleField)
 {
+  // original any_value
   sup::dto::AnyValue any_value = {{{"signed", {sup::dto::SignedInteger32Type, 42}}}};
 
+  // initializing builder
   auto pvxs_type = ::pvxs::TypeDef(::pvxs::TypeCode::Struct, {pvxs::members::Int32("signed")});
   PvxsValueBuilder builder(pvxs_type);
 
+  // building PVXS value
   builder.StructProlog(&any_value);
   builder.MemberProlog(&any_value["signed"], "signed");
   builder.ScalarProlog(&any_value["signed"]);
@@ -85,6 +92,7 @@ TEST_F(PvxsValueBuilderTests, StructWithSingleField)
 
   auto pvxs_value = builder.GetPVXSValue();
 
+  // validating pvxs_value
   EXPECT_EQ(pvxs_value.type(), ::pvxs::TypeCode::Struct);
   EXPECT_EQ(pvxs_value.nmembers(), 1);
 
@@ -98,13 +106,16 @@ TEST_F(PvxsValueBuilderTests, StructWithSingleField)
 
 TEST_F(PvxsValueBuilderTests, StructWithTwoFields)
 {
+  // original any_value
   sup::dto::AnyValue any_value = {{"signed", {sup::dto::SignedInteger32Type, 42}},
                                   {"bool", {sup::dto::BooleanType, true}}};
 
+  // initializing builder
   auto pvxs_type = ::pvxs::TypeDef(::pvxs::TypeCode::Struct,
                                    {pvxs::members::Int32("signed"), pvxs::members::Bool("bool")});
   PvxsValueBuilder builder(pvxs_type);
 
+  // building PVXS value
   builder.StructProlog(&any_value);
   builder.MemberProlog(&any_value["signed"], "signed");
   builder.ScalarProlog(&any_value["signed"]);
@@ -119,6 +130,7 @@ TEST_F(PvxsValueBuilderTests, StructWithTwoFields)
 
   auto pvxs_value = builder.GetPVXSValue();
 
+  // validating pvxs_value
   EXPECT_EQ(pvxs_value.type(), ::pvxs::TypeCode::Struct);
   EXPECT_EQ(pvxs_value.nmembers(), 2);
 
@@ -129,4 +141,61 @@ TEST_F(PvxsValueBuilderTests, StructWithTwoFields)
   EXPECT_EQ(pvxs_value["signed"].as<int32_t>(), 42);
   EXPECT_EQ(pvxs_value["bool"].type(), ::pvxs::TypeCode::Bool);
   EXPECT_EQ(pvxs_value["bool"].as<bool>(), true);
+}
+
+//! Build PVXS value from a structure with two fields inside parent structure.
+
+TEST_F(PvxsValueBuilderTests, NestedStruct)
+{
+  // original any_value
+  sup::dto::AnyValue two_scalars = {{"signed", {sup::dto::SignedInteger32Type, 42}},
+                                    {"bool", {sup::dto::BooleanType, true}}};
+  sup::dto::AnyValue any_value = {{"scalars", two_scalars}};
+
+  // initialising builder
+  auto pvxs_type = ::pvxs::TypeDef(::pvxs::TypeCode::Struct,
+                              {pvxs::members::Struct("scalars", {pvxs::members::Int32("signed"),                                                                pvxs::members::Bool("bool")})});
+  PvxsValueBuilder builder(pvxs_type);
+
+  // building PVXS value
+  builder.StructProlog(&any_value);
+  builder.MemberProlog(&any_value["scalars"], "scalars");
+
+  builder.StructProlog(&any_value["scalars"]);
+  builder.MemberProlog(&any_value["scalars.signed"], "signed");
+  builder.ScalarProlog(&any_value["scalars.signed"]);
+  builder.ScalarEpilog(&any_value["scalars.signed"]);
+  builder.MemberEpilog(&any_value["scalars.signed"], "signed");
+  builder.StructMemberSeparator();
+  builder.MemberProlog(&any_value["scalars.bool"], "bool");
+  builder.ScalarProlog(&any_value["scalars.bool"]);
+  builder.ScalarEpilog(&any_value["scalars.bool"]);
+  builder.MemberEpilog(&any_value["scalars.bool"], "bool");
+  builder.StructEpilog(&any_value["scalars"]);
+
+  builder.MemberEpilog(&any_value["scalars"], "scalars");
+  builder.StructEpilog(&any_value);
+
+  auto pvxs_value = builder.GetPVXSValue();
+
+  // validating pvxs_value
+  EXPECT_EQ(pvxs_value.type(), ::pvxs::TypeCode::Struct);
+  EXPECT_EQ(pvxs_value.nmembers(), 1);
+
+  auto names = GetMemberNames(pvxs_value);
+  EXPECT_EQ(names, std::vector<std::string>({"scalars"}));
+
+  auto nested_value = pvxs_value["scalars"];
+  EXPECT_EQ(nested_value.type(), ::pvxs::TypeCode::Struct);
+  EXPECT_EQ(nested_value.nmembers(), 2);
+
+  auto nested_names = GetMemberNames(nested_value);
+  EXPECT_EQ(nested_names, std::vector<std::string>({"signed", "bool"}));
+
+  EXPECT_EQ(pvxs_value["scalars.signed"].type(), ::pvxs::TypeCode::Int32);
+  EXPECT_EQ(pvxs_value["scalars.bool"].type(), ::pvxs::TypeCode::Bool);
+
+  EXPECT_EQ(pvxs_value["scalars.signed"].as<int32_t>(), 42);
+  EXPECT_EQ(pvxs_value["scalars.bool"].as<bool>(), true);
+
 }
