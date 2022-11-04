39346dd560078b033eb5c0ee65135ed6f696fd1e
Walter Van Herck <walter.vanherck@iter.org>
2022-11-04 15:16:39 +0000

    First unit tests for RPC working

--- src/test/c++/unit/pv_access_rpc_tests.cpp
+++ src/test/c++/unit/pv_access_rpc_tests.cpp
@@ -17,19 +17,15 @@
  * of the distribution package.
  *****************************************************************************/
 
-#include "mock_utils.h"
-#include "unit_test_helper.h"
-
 #include <sup/epics/pv_access_rpc_client.h>
 #include <sup/epics/pv_access_rpc_server.h>
 
-#include <gtest/gtest.h>
 #include <sup/dto/anyvalue.h>
+#include <sup/rpc/protocol_rpc.h>
 
-#include <thread>
+#include <gtest/gtest.h>
 
-using ::testing::_;
-using sup::epics::unit_test_helper::BusyWaitFor;
+#include <functional>
 
 using namespace sup::epics;
 
@@ -37,12 +33,90 @@ using namespace sup::epics;
 
 class PvAccessRPCTests : public ::testing::Test
 {
+protected:
+  PvAccessRPCTests();
+  ~PvAccessRPCTests();
+
+  std::unique_ptr<sup::dto::AnyFunctor> CreateHandler();
+
+  std::unique_ptr<sup::dto::AnyValue> m_request;
+  std::unique_ptr<sup::dto::AnyValue> m_reply;
+};
+
+class TestHandler : public sup::dto::AnyFunctor
+{
 public:
+  TestHandler(std::function<void(const sup::dto::AnyValue&,const sup::dto::AnyValue&)> func);
+  ~TestHandler();
+
+  sup::dto::AnyValue operator()(const sup::dto::AnyValue& request) override;
+private:
+  std::function<void(const sup::dto::AnyValue&,const sup::dto::AnyValue&)> m_func;
 };
 
 //! Standard scenario. Single server and single client.
 
-TEST_F(PvAccessRPCTests, SingleServerSingleClient)
+TEST_F(PvAccessRPCTests, SingleServerSingleClientSuccess)
 {
+  std::string channel_name = "PvAccessRPCTests:channel";
+  PvAccessRPCServer server(PvAccessRPCServer::Isolated, GetDefaultRPCServerConfig(channel_name),
+                           CreateHandler());
+  auto client = server.CreateClient(GetDefaultRPCClientConfig(channel_name));
+
+  // Send simple scalar payload over RPC
+  sup::dto::AnyValue payload{42};
+  auto request = sup::rpc::utils::CreateRPCRequest(payload);
+  auto reply = client(request);
+  EXPECT_TRUE(sup::rpc::utils::CheckReplyFormat(reply));
+  ASSERT_TRUE(static_cast<bool>(m_request));
+  ASSERT_TRUE(static_cast<bool>(m_reply));
+  EXPECT_EQ(request, *m_request);
+  EXPECT_EQ(reply, *m_reply);
 }
 
+//! Fail at client side. Message should not arrive at server.
+
+TEST_F(PvAccessRPCTests, RPCClientErrors)
+{
+  std::string channel_name = "PvAccessRPCTests:channel";
+  PvAccessRPCServer server(PvAccessRPCServer::Isolated, GetDefaultRPCServerConfig(channel_name),
+                           CreateHandler());
+  auto client = server.CreateClient(GetDefaultRPCClientConfig(channel_name));
+
+  // Send empty value
+  sup::dto::AnyValue empty{};
+  auto reply = client(empty);
+  EXPECT_FALSE(sup::rpc::utils::CheckReplyFormat(reply));
+  EXPECT_TRUE(sup::dto::IsEmptyValue(reply));
+  EXPECT_FALSE(static_cast<bool>(m_request));
+  EXPECT_FALSE(static_cast<bool>(m_reply));
+}
+
+PvAccessRPCTests::PvAccessRPCTests()
+  : m_request{}
+  , m_reply{}
+{}
+
+PvAccessRPCTests::~PvAccessRPCTests() = default;
+
+std::unique_ptr<sup::dto::AnyFunctor> PvAccessRPCTests::CreateHandler()
+{
+  return std::unique_ptr<sup::dto::AnyFunctor>{new TestHandler(
+    [this](const sup::dto::AnyValue& request, const sup::dto::AnyValue& reply){
+      m_request.reset(new sup::dto::AnyValue(request));
+      m_reply.reset(new sup::dto::AnyValue(reply));
+    })};
+}
+
+TestHandler::TestHandler(std::function<void(const sup::dto::AnyValue&,const sup::dto::AnyValue&)> func)
+  : m_func{std::move(func)}
+{}
+
+TestHandler::~TestHandler() = default;
+
+sup::dto::AnyValue TestHandler::operator()(const sup::dto::AnyValue& request)
+{
+  auto reply = sup::rpc::utils::CreateRPCReply(sup::rpc::Success);
+  m_func(request, reply);
+  return reply;
+}
