c401041af58730740b7fa0f1b47d38f53dc985c5
Rafael Graça Silva <rafael.g.silva@criticalsoftware.com>
2023-01-30 14:03:28 +0000

    move cmake sources into cmake root directory

--- cmake/module/installation.cmake
+++ cmake/module/installation.cmake
@@ -29,7 +29,7 @@ endif()
 include(CMakePackageConfigHelpers)
 
 # to use in the build tree
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/sup-epics-config.cmake.in
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/../config/sup-epics-config.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/sup-epics-config.cmake
     INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
 )
