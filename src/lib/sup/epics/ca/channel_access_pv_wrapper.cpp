630092168bd49b9ceb8fa5a770e2c7328d3c8448
Walter Van Herck <walter.vanherck@iter.org>
2024-02-06 15:04:46 +0000

    Add wrapper of ChannelAccessPV for factory

---
+++ src/lib/sup/epics/ca/channel_access_pv_wrapper.cpp
@@ -0,0 +1,86 @@
+/******************************************************************************
+ *
+ * Project       : Supervision and automation system EPICS interface
+ *
+ * Description   : Library of SUP components for EPICS network protocol
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "channel_access_pv_wrapper.h"
+
+#include <sup/protocol/exceptions.h>
+
+namespace sup
+{
+namespace epics
+{
+ChannelAccessPVWrapper::ChannelAccessPVWrapper(const std::string& channel,
+                                               const sup::dto::AnyType& type)
+  : m_callback{}
+  , m_pv_impl{}
+{
+  auto callback = [this](const ChannelAccessPV::ExtendedValue& val){
+    return OnUpdate(val);
+  };
+  m_pv_impl.reset(new ChannelAccessPV(channel, type, callback));
+}
+
+ChannelAccessPVWrapper::~ChannelAccessPVWrapper() = default;
+
+bool ChannelAccessPVWrapper::IsAvailable() const
+{
+  return m_pv_impl->WaitForValidValue(0.0);
+}
+
+sup::dto::AnyValue ChannelAccessPVWrapper::GetValue(double timeout_sec) const
+{
+  if (!m_pv_impl->WaitForValidValue(timeout_sec))
+  {
+    std::string error = "Variable could not be accessed within timeout";
+    throw sup::protocol::VariableUnavailableException(error);
+  }
+  return m_pv_impl->GetValue();
+}
+
+bool ChannelAccessPVWrapper::SetValue(const sup::dto::AnyValue& value, double timeout_sec)
+{
+  if (!m_pv_impl->WaitForConnected(timeout_sec))
+  {
+    return false;
+  }
+  return m_pv_impl->SetValue(value);
+}
+
+bool ChannelAccessPVWrapper::WaitForAvailable(double timeout_sec) const
+{
+  return m_pv_impl->WaitForValidValue(timeout_sec);
+}
+
+bool ChannelAccessPVWrapper::SetMonitorCallback(Callback func)
+{
+  m_callback = func;
+  return true;
+}
+
+void ChannelAccessPVWrapper::OnUpdate(const ChannelAccessPV::ExtendedValue& val)
+{
+  if (m_callback)
+  {
+    m_callback(val.value, val.connected);
+  }
+}
+
+}  // namespace epics
+
+}  // namespace sup
---
+++ src/lib/sup/epics/ca/channel_access_pv_wrapper.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ *
+ * Project       : Supervision and automation system EPICS interface
+ *
+ * Description   : Library of SUP components for EPICS network protocol
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUP_EPICS_CHANNEL_ACCESS_PV_WRAPPER_H_
+#define SUP_EPICS_CHANNEL_ACCESS_PV_WRAPPER_H_
+
+#include <sup/epics/channel_access_pv.h>
+
+#include <sup/protocol/process_variable.h>
+
+namespace sup
+{
+namespace epics
+{
+
+class ChannelAccessPVWrapper : public sup::protocol::ProcessVariable
+{
+public:
+  ChannelAccessPVWrapper(const std::string& channel, const sup::dto::AnyType& type);
+  ~ChannelAccessPVWrapper();
+
+  bool IsAvailable() const override;
+  sup::dto::AnyValue GetValue(double timeout_sec) const override;
+  bool SetValue(const sup::dto::AnyValue& value, double timeout_sec) override;
+  bool WaitForAvailable(double timeout_sec) const override;
+  bool SetMonitorCallback(Callback func) override;
+private:
+  void OnUpdate(const ChannelAccessPV::ExtendedValue& val);
+  sup::protocol::ProcessVariable::Callback m_callback;
+  std::unique_ptr<ChannelAccessPV> m_pv_impl;
+};
+
+}  // namespace epics
+
+}  // namespace sup
+
+#endif  // SUP_EPICS_CHANNEL_ACCESS_PV_WRAPPER_H_
