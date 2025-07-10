0922f261cc144f65c2d96e830f49fe9b3d35ab3e
Walter Van Herck <walter.vanherck@iter.org>
2025-07-10 12:52:48 +0000

    Update file headers

--- tools/src/rpc_sniff_server.cpp
+++ tools/src/rpc_sniff_server.cpp
@@ -1,21 +1,22 @@
 /******************************************************************************
  *
- * Project       : COA testing
+ * Project       : Supervision and automation system EPICS interface
  *
- * Description   : COA functional and integration tests
+ * Description   : Library of SUP components for EPICS network protocol
  *
- * Author        : Ricardo Torres (Ext)
+ * Author        : Walter Van Herck (IO)
  *
  * Copyright (c) : 2010-2025 ITER Organization,
  *                 CS 90 046
  *                 13067 St. Paul-lez-Durance Cedex
  *                 France
+ * SPDX-License-Identifier: MIT
  *
  * This file is part of ITER CODAC software.
  * For the terms and conditions of redistribution or use of this software
- * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * refer to the file LICENSE located in the top level directory
  * of the distribution package.
- ******************************************************************************/
+ *****************************************************************************/
 
 #include "rpc_sniff_server.h"
 
--- tools/src/rpc_sniff_server.h
+++ tools/src/rpc_sniff_server.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+ *
+ * Project       : Supervision and automation system EPICS interface
+ *
+ * Description   : Library of SUP components for EPICS network protocol
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
 #include <sup/dto/anyvalue.h>
 #include <sup/dto/any_functor.h>
 
