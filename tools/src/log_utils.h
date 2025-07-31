c5025a2277435c9352344f0c3bd3e7c287a57773
Walter Van Herck <walter.vanherck@iter.org>
2025-07-31 15:59:56 +0000

    Add log utils

---
+++ tools/src/log_utils.h
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *
+ * Project       : Supervision and automation system EPICS interface
+ *
+ * Description   : Library of SUP components for EPICS network protocol
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include <sup/dto/anyvalue.h>
+
+#include <string>
+
+void PrintAnyvaluePacket(const std::string& title, const sup::dto::AnyValue& value);
