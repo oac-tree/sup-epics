6e912cff5f5811270f3be8475d35ec3aab402377
Walter Van Herck <walter.vanherck@iter.org>
2025-08-04 09:03:59 +0000

    Create unit test for logging RPC client/server

--- test/unit/pv_access_logging_client_server_tests.cpp
+++ test/unit/pv_access_logging_client_server_tests.cpp
@@ -21,19 +21,87 @@
 #include <gtest/gtest.h>
 
 #include <sup/epics/epics_protocol_factory.h>
+#include <sup/epics/pv_access_rpc_client.h>
+
+#include <sup/protocol/log_any_functor_decorator.h>
+
+using namespace sup::epics;
+
+class TestFunctor : public sup::dto::AnyFunctor
+{
+public:
+  explicit TestFunctor(sup::dto::AnyValue fixed_reply)
+      : m_fixed_reply(std::move(fixed_reply))
+  {}
+
+  sup::dto::AnyValue operator()(const sup::dto::AnyValue&) override
+  {
+    return m_fixed_reply;
+  }
+private:
+  sup::dto::AnyValue m_fixed_reply;
+};
 
 class PvAccessLoggingClientServerTests : public ::testing::Test
 {
-public:
+protected:
 
   PvAccessLoggingClientServerTests() = default;
   ~PvAccessLoggingClientServerTests() override = default;
+
+  std::vector<sup::dto::AnyValue> m_client_packages_sent;
+  std::vector<sup::dto::AnyValue> m_client_packages_received;
+  std::vector<sup::dto::AnyValue> m_server_packages_sent;
+  std::vector<sup::dto::AnyValue> m_server_packages_received;
 };
 
-//! Initial state of PvAccessClientPV when no server exists.
-
-TEST_F(PvAccessLoggingClientServerTests, ScalarPayload)
+TEST_F(PvAccessLoggingClientServerTests, PacketLogging)
 {
-  EXPECT_TRUE(true);
+  using PacketDirection = sup::protocol::LogAnyFunctorDecorator::PacketDirection;
+  auto client_log_function = [this](const sup::dto::AnyValue& packet, PacketDirection direction)
+  {
+    if (direction == PacketDirection::kInput)
+    {
+      m_client_packages_sent.push_back(packet);
+    }
+    else
+    {
+      m_client_packages_received.push_back(packet);
+    }
+  };
+  auto server_log_function = [this](const sup::dto::AnyValue& packet, PacketDirection direction)
+  {
+    if (direction == PacketDirection::kInput)
+    {
+      m_server_packages_received.push_back(packet);
+    }
+    else
+    {
+      m_server_packages_sent.push_back(packet);
+    }
+  };
+  sup::dto::AnyValue request = {{
+    { "setpoint", { sup::dto::Float64Type, 3.14 }},
+    { "enabled", false }
+  }};
+  sup::dto::AnyValue reply = {{
+    { "counter", { sup::dto::UnsignedInteger16Type, 42u }},
+    { "message", "ok" }
+  }};
+  TestFunctor fixed_reply_functor(reply);
+  std::string server_name = "LoggingClientServerTest::Server";
+  sup::epics::PvAccessRPCServerConfig server_config{server_name};
+  auto client_config = sup::epics::GetDefaultRPCClientConfig(server_name);
+  auto server = CreateLoggingEPICSRPCServer(server_config, fixed_reply_functor, server_log_function);
+  auto client = CreateLoggingEPICSRPCClient(client_config, client_log_function);
+  client->operator()(request);
+  EXPECT_EQ(m_client_packages_sent.size(), 1);
+  EXPECT_EQ(m_client_packages_received.size(), 1);
+  EXPECT_EQ(m_server_packages_received.size(), 1);
+  EXPECT_EQ(m_server_packages_sent.size(), 1);
+  EXPECT_EQ(m_client_packages_sent[0], request);
+  EXPECT_EQ(m_client_packages_received[0], reply);
+  EXPECT_EQ(m_server_packages_received[0], request);
+  EXPECT_EQ(m_server_packages_sent[0], reply);
 }
 
