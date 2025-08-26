0b8ee0a45289323ded8af8da50848047a70ea681
Walter Van Herck <walter.vanherck@iter.org>
2025-08-26 10:10:19 +0000

    Add unit test for HandleRPCCall

--- test/unit/pv_access_rpc_utils_tests.cpp
+++ test/unit/pv_access_rpc_utils_tests.cpp
@@ -20,7 +20,6 @@
 
 #include <sup/epics/pv_access_rpc_client.h>
 #include <sup/epics/pv_access_rpc_server.h>
-#include <sup/epics/rpc/pv_access_rpc_utils.h>
 #include <sup/epics-test/unit_test_helper.h>
 
 #include <sup/dto/anyvalue.h>
@@ -60,6 +59,29 @@ TEST_F(PvAccessRPCUtilsTests, ClientRPCCall)
   auto reply = client(payload);
   EXPECT_TRUE(sup::protocol::utils::CheckReplyFormat(reply));
   EXPECT_EQ(reply[sup::protocol::constants::REPLY_RESULT].As<sup::dto::uint32>(), 2);
+}
+
+TEST_F(PvAccessRPCUtilsTests, HandleRPCCall)
+{
+  std::string channel_name = "PvAccessRPCUtilsTests_2";
+  // Array of structures payload fails
+  // building any value
+  sup::dto::AnyValue struct_value1 = {{{"field_name", {sup::dto::SignedInteger32Type, 42}}},
+                                      "struct_name"};
+  sup::dto::AnyValue struct_value2 = {{{"field_name", {sup::dto::SignedInteger32Type, 43}}},
+                                      "struct_name"};
+  auto fixed_reply = sup::dto::ArrayValue({struct_value1, struct_value2});
+  test::FixedReplyFunctor handler{ fixed_reply };
+  PvAccessRPCServer server(PvAccessRPCServer::Isolated, GetDefaultRPCServerConfig(channel_name),
+                           handler);
+  auto client = server.CreateClient(GetDefaultRPCClientConfig(channel_name));
+
+  sup::dto::AnyValue payload = {{
+    {"result", { sup::dto::UnsignedInteger32Type, 42 } }
+  }};
+  auto reply = client(payload);
+  EXPECT_TRUE(sup::protocol::utils::CheckReplyFormat(reply));
+  EXPECT_EQ(reply[sup::protocol::constants::REPLY_RESULT].As<sup::dto::uint32>(), 3);
 }
 
 PvAccessRPCUtilsTests::PvAccessRPCUtilsTests() = default;
